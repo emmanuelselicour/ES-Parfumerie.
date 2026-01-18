fa257ca1c3cd672460f30fd089949723cdcaf83b
emmanuelselicour <emmanuelselicour.2002@gmail.com>
2026-01-17 20:10:14 -0500

    Create Footer.js

---
+++ public/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="w-full bg-gray-900 text-white py-6 mt-10">
+      <div className="container mx-auto text-center">
+        <p>© 2025 ES Parfumerie - Tous droits réservés</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
